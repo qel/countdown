07ac656c67736ed88016ddb2ae330e624dae8a49
Raquel Foster <rqleve@gmail.com>
2016-07-17 14:18:50 -0500

    feature: initial commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: __dirname + '/src/index.jsx',
+    output: {
+        filename: 'index.js',
+        path: __dirname + '/lib',
+        publicPath: '/lib'
+    },
+    module: {
+        loaders: [
+            {
+                test: /(\.jsx|\.js)$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /(\.jsx|\.js)$/,
+                loader: "eslint-loader",
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        root: path.resolve('./src'),
+        extensions: ['', '.js']
+    }
+};
