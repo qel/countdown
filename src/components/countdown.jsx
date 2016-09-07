dbca76156d1b5e19cbacc726c261205f670452c0
Raquel Foster <rqleve@gmail.com>
2016-09-07 18:37:25 -0500

    feature: switch back to ticking seconds

--- src/components/countdown.jsx
+++ src/components/countdown.jsx
@@ -86,12 +86,7 @@ class Countdown extends Component {
                 {offsetMessage}
                 <Dial radius={44} stroke={6} pos={props.hours} prevPos={props.prev.hours} max={24} />
                 <Dial radius={36} stroke={6} pos={props.minutes} prevPos={props.prev.minutes} max={60} />
-                <Dial
-                    radius={28}
-                    stroke={6}
-                    pos={props.seconds + props.milliseconds / 1000}
-                    prevPos={props.prev.seconds} max={60}
-                />
+                <Dial radius={28} stroke={6}pos={props.seconds} prevPos={props.prev.seconds} max={60} />
                 <Dial radius={20} stroke={6} pos={props.milliseconds} prevPos={props.prev.milliseconds} max={1000} />
             </div>
         );
